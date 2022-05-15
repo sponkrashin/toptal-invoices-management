d4ba7a2b8105a20eb1b55a6ab48b6a2ec967b066
Sergei Ponkrashin <ponkrashin.sv@gmail.com>
2022-05-15 11:09:54 +0300

    feat: Add navigation links to sidebar
    Resolves: #5

--- src/components/layout/SidebarItems.js
+++ src/components/layout/SidebarItems.js
@@ -5,23 +5,32 @@ import ListItemText from '@mui/material/ListItemText';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import PeopleIcon from '@mui/icons-material/People';
 import BarChartIcon from '@mui/icons-material/BarChart';
+import { Link, useMatch } from 'react-router-dom';
 
 export default function SidebarItems() {
+  const isDashboardActive = useMatch({ path: '/', end: true }) !== null;
+  const isInvoicesActive = useMatch({ path: '/invoices', end: true }) !== null;
+  const isClientsActive = useMatch({ path: '/clients', end: true }) !== null;
+
   return (
     <List component="nav">
-      <ListItemButton selected={true}>
+      <ListItemButton selected={isDashboardActive} component={Link} to="/">
         <ListItemIcon>
           <DashboardIcon />
         </ListItemIcon>
         <ListItemText primary="Dashboard" />
       </ListItemButton>
-      <ListItemButton>
+      <ListItemButton
+        selected={isInvoicesActive}
+        component={Link}
+        to="/invoices"
+      >
         <ListItemIcon>
           <BarChartIcon />
         </ListItemIcon>
         <ListItemText primary="Invoices" />
       </ListItemButton>
-      <ListItemButton>
+      <ListItemButton selected={isClientsActive} component={Link} to="/clients">
         <ListItemIcon>
           <PeopleIcon />
         </ListItemIcon>
