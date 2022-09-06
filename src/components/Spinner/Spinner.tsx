a23705dd96aa412fd25af804135244abd1c3417f
Sergei Ponkrashin <ponkrashin.sv@gmail.com>
2022-09-07 00:01:22 +0300

    feat: Add ability to show spinner above the container where it is located

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,13 +1,27 @@
-import { CircularProgress } from '@mui/material';
+import { CircularProgress, Backdrop } from '@mui/material';
+import styles from './Spinner.module.scss';
 
 export interface SpinnerProps {
   size?: 'small' | 'large';
   spinning?: boolean;
+  inContainer?: boolean;
 }
 
-const Spinner = ({ size, spinning }: SpinnerProps) => {
+const Spinner = ({ size, spinning, inContainer }: SpinnerProps) => {
+  if (!spinning) {
+    return null;
+  }
+
   const sizeInPx = size === 'large' ? 30 : 20;
-  return spinning !== false ? <CircularProgress size={sizeInPx} /> : null;
+  const spinner = <CircularProgress size={sizeInPx} />;
+
+  return inContainer ? (
+    <Backdrop className={styles.container} open>
+      {spinner}
+    </Backdrop>
+  ) : (
+    spinner
+  );
 };
 
 export default Spinner;
