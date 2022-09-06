911dff6758570aa7ddfc42d78a6c82afd93a5550
Sergei Ponkrashin <ponkrashin.sv@gmail.com>
2022-09-06 15:10:01 +0300

    feat: Initialize auth when the application starts

--- src/components/AppWrapper/AppWrapper.tsx
+++ src/components/AppWrapper/AppWrapper.tsx
@@ -1,18 +1,29 @@
+import { useEffect } from 'react';
 import { AppProps } from 'next/app';
 import { Box, Container, Toolbar } from '@mui/material';
 import HeaderWithSidebarContainer from 'components/HeaderWithSidebarContainer';
+import { initializeAuth } from 'store/authSlice';
+import { useDispatch } from 'store/hooks';
 import styles from './AppWrapper.module.scss';
 
-const AppWrapper = ({ Component, pageProps }: AppProps) => (
-  <Box className={styles.wrapper}>
-    <HeaderWithSidebarContainer />
-    <Box className={styles.mainContainer} component="main">
-      <Toolbar />
-      <Container className={styles.content} maxWidth="lg">
-        <Component {...pageProps} />
-      </Container>
+const AppWrapper = ({ Component, pageProps }: AppProps) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(initializeAuth());
+  }, [dispatch]);
+
+  return (
+    <Box className={styles.wrapper}>
+      <HeaderWithSidebarContainer />
+      <Box className={styles.mainContainer} component="main">
+        <Toolbar />
+        <Container className={styles.content} maxWidth="lg">
+          <Component {...pageProps} />
+        </Container>
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
 
 export default AppWrapper;
--- src/store/authSlice.ts
+++ src/store/authSlice.ts
@@ -1,4 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import * as apiService from 'data/apiService';
+import { HttpError } from 'data/httpError';
 import * as authTokenStorage from 'services/authTokenStorage';
 import type { AppState, AppThunk } from './rootStore';
 
@@ -28,6 +30,17 @@ export const authSlice = createSlice({
 });
 
 const { signInInternal, signOutInternal } = authSlice.actions;
+
+export const initializeAuth = (): AppThunk => async (dispatch) => {
+  try {
+    const currentUser = await apiService.getCurrentUser();
+    dispatch(signInInternal(currentUser.name));
+  } catch (err: any) {
+    if (err instanceof HttpError && err.status === 401) {
+      dispatch(signOutInternal());
+    }
+  }
+};
 
 export const signIn =
   (username: string, authToken: string): AppThunk =>
