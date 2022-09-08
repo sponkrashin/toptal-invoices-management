03ca096535621e2e2e59ed1401c33ef14934479e
Sergei Ponkrashin <ponkrashin.sv@gmail.com>
2022-09-08 11:08:03 +0300

    feat: Add getting of current id from query params

--- src/modules/ViewClient/ViewClient.tsx
+++ src/modules/ViewClient/ViewClient.tsx
@@ -1,2 +1,8 @@
-const ViewClient = () => <div>View client page</div>;
+import { useAppRouter } from 'hooks/useAppRouter';
+
+const ViewClient = () => {
+  const { router } = useAppRouter();
+  return <div>View client page with id {router.query['id']}</div>;
+};
+
 export default ViewClient;
--- src/modules/ViewInvoice/ViewInvoice.tsx
+++ src/modules/ViewInvoice/ViewInvoice.tsx
@@ -1,2 +1,8 @@
-const ViewInvoice = () => <div>View invoice page</div>;
+import { useAppRouter } from 'hooks/useAppRouter';
+
+const ViewInvoice = () => {
+  const { router } = useAppRouter();
+  return <div>View invoice page with id {router.query['id']}</div>;
+};
+
 export default ViewInvoice;
