b44757494624451b759534283d9c451d3c618361
Sergei Ponkrashin <ponkrashin.sv@gmail.com>
2022-09-08 10:40:11 +0300

    feat: Refactor header to remove drawer collapse button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,11 @@
-import { MouseEventHandler } from 'react';
-import PropTypes from 'prop-types';
-import MenuIcon from '@mui/icons-material/Menu';
-import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography } from '@mui/material';
 import { useSelector, useDispatch } from 'store/hooks';
 import { selectUserIsLoggedIn, selectUserName, signOut } from 'store/userInfoSlice';
 import AnonymousUser from '../AnonymousUser';
 import LoggedUser from '../LoggedUser';
 import styles from './Header.module.scss';
 
-export interface HeaderProps {
-  onToggleDrawer: MouseEventHandler<HTMLButtonElement>;
-}
-
-const Header = ({ onToggleDrawer }: HeaderProps) => {
+const Header = () => {
   const userIsLoggedIn = useSelector(selectUserIsLoggedIn);
   const userName = useSelector(selectUserName);
   const dispatch = useDispatch();
@@ -28,27 +21,13 @@ const Header = ({ onToggleDrawer }: HeaderProps) => {
   return (
     <AppBar className={styles.appBar} position="absolute">
       <Toolbar>
-        <IconButton
-          className={styles.drawerButton}
-          edge="start"
-          color="inherit"
-          aria-label="open drawer"
-          onClick={onToggleDrawer}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography className={styles.title} component="h1" variant="h6" color="inherit" noWrap>
+        <Typography className={styles.title} component="h1" variant="h6" noWrap>
           Invoices Management
         </Typography>
         {userComponent}
       </Toolbar>
     </AppBar>
   );
-};
-
-Header.propTypes = {
-  open: PropTypes.bool,
-  toggleDrawer: PropTypes.func,
 };
 
 export default Header;
