c44088acfb0dd5b51532c7337e8587e4d1c1741c
Sergei Ponkrashin <ponkrashin.sv@gmail.com>
2022-09-08 11:42:58 +0300

    feat: Add navigation to view client and view invoice pages when user clicks on a table row in a dashboard page

--- src/modules/Dashboard/Dashboard.tsx
+++ src/modules/Dashboard/Dashboard.tsx
@@ -3,6 +3,9 @@ import Card from 'components/Card';
 import ClientsTable from 'components/ClientsTable';
 import InvoicesTable from 'components/InvoicesTable';
 import Spinner from 'components/Spinner';
+import { Client } from 'data/client';
+import { Invoice } from 'data/invoice';
+import { NavigationTarget, useAppRouter } from 'hooks/useAppRouter';
 import { useClients } from 'hooks/useClients';
 import { useInvoices } from 'hooks/useInvoices';
 import styles from './Dashboard.module.scss';
@@ -10,6 +13,7 @@ import styles from './Dashboard.module.scss';
 const Dashboard = () => {
   const { data: clients, isLoading: clientsLoading } = useClients();
   const { data: invoices, isLoading: invoicesLoading } = useInvoices();
+  const { navigate } = useAppRouter();
 
   const filteredInvoices = invoices
     .sort((a, b) => b.date.valueOf() - a.date.valueOf())
@@ -19,18 +23,21 @@ const Dashboard = () => {
 
   const filteredClients = clients.filter((inv) => !!filteredInvoicesByClientId[inv.id]);
 
+  const handleClientsRowClick = (row: Client) => navigate(NavigationTarget.ViewClient, row.id);
+  const handleInvoicesRowClick = (row: Invoice) => navigate(NavigationTarget.ViewInvoce, row.id);
+
   return (
     <>
       <Card title="Clients" contentClassName={`${clientsLoading ? styles.cardContentWithLoading : ''}`}>
         <Spinner size="large" spinning={clientsLoading} />
         {!clientsLoading && (
-          <ClientsTable data={filteredClients} onRowClick={(row) => console.log('From dashboard', row)} />
+          <ClientsTable data={filteredClients} onRowClick={(row) => handleClientsRowClick(row as any)} />
         )}
       </Card>
       <Card title="Invoices" contentClassName={`${invoicesLoading ? styles.cardContentWithLoading : ''}`}>
         <Spinner size="large" spinning={invoicesLoading} />
         {!invoicesLoading && (
-          <InvoicesTable data={filteredInvoices} onRowClick={(row) => console.log('From dashboard', row)} />
+          <InvoicesTable data={filteredInvoices} onRowClick={(row) => handleInvoicesRowClick(row as any)} />
         )}
       </Card>
     </>
