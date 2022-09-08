b4e8bf8de8b72a32c53dd5584fe701b08b39b4a2
Sergei Ponkrashin <ponkrashin.sv@gmail.com>
2022-09-08 11:27:09 +0300

    feat: Add router method to navigate to view client and view invoice pages

--- src/hooks/useAppRouter.ts
+++ src/hooks/useAppRouter.ts
@@ -6,16 +6,30 @@ export const INVOICES_URL = '/invoices';
 export const COMPANY_DETAILS_URL = '/company-details';
 export const SIGN_IN_URL = '/login';
 export const SIGN_UP_URL = '/sign-up';
+export const VIEW_CLIENT_URL = '/clients/{id}';
+export const VIEW_INVOICE_URL = '/invoices/{id}/view';
 
 export enum NavigationTarget {
   Dashboard = 'Dashboard',
   SignIn = 'SignIn',
   CompanyDetails = 'CompanyDetails',
+  ViewClient = 'ViewClient',
+  ViewInvoce = 'ViewInvoce',
 }
 
 const ID_QUERY_PARAM = 'id';
 
-function navigate(router: NextRouter, target: NavigationTarget): Promise<boolean> {
+function formatUrl(url: string, params: Record<string, string>): string {
+  let result = url;
+
+  for (const key in params) {
+    result = result.replace(`{${key}}`, params[key]);
+  }
+
+  return result;
+}
+
+function navigate(router: NextRouter, target: NavigationTarget, id?: number): Promise<boolean> {
   switch (target) {
     case NavigationTarget.Dashboard:
       return router.push(DASHBOARD_URL);
@@ -26,6 +40,20 @@ function navigate(router: NextRouter, target: NavigationTarget): Promise<boolean
     case NavigationTarget.CompanyDetails:
       return router.push(COMPANY_DETAILS_URL);
 
+    case NavigationTarget.ViewClient:
+      if (!id) {
+        throw new Error('Id parameter is required');
+      }
+
+      return router.push(formatUrl(VIEW_CLIENT_URL, { [ID_QUERY_PARAM]: id.toString() }));
+
+    case NavigationTarget.ViewInvoce:
+      if (!id) {
+        throw new Error('Id parameter is required');
+      }
+
+      return router.push(formatUrl(VIEW_INVOICE_URL, { [ID_QUERY_PARAM]: id.toString() }));
+
     default:
       throw new Error(`Navigation target ${target} is not supported yet`);
   }
@@ -34,7 +62,7 @@ function navigate(router: NextRouter, target: NavigationTarget): Promise<boolean
 export function useAppRouter(): {
   router: NextRouter;
   idQueryParam: number | null;
-  navigate: (target: NavigationTarget) => Promise<boolean>;
+  navigate: (target: NavigationTarget, id?: number) => Promise<boolean>;
 } {
   const router = useRouter();
 
@@ -43,6 +71,6 @@ export function useAppRouter(): {
   return {
     router,
     idQueryParam,
-    navigate: (target) => navigate(router, target),
+    navigate: (target, id) => navigate(router, target, id),
   };
 }
